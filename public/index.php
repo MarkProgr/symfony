2270d7a96f3d6bb3a8e11737f1977483648755a7
MarkProgr <kristianomark7@gmail.com>
2022-12-15 15:57:38 +0100

    init aws + symf

--- public/index.php
+++ public/index.php
@@ -5,5 +5,10 @@ use App\Kernel;
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
+    $s3Client = new \Aws\S3\S3Client([
+        ''
+    ]);
+    $s3Client->registerStreamWrapper();
+
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
